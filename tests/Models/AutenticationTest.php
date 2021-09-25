ba86279abd2a5d71db2d8947a19ac4b4db2da743
Ellyzeul <gabrielaug.240@gmail.com>
2021-09-25 18:15:30 -0300

    Completando testes em Autentication::login

--- tests/Models/AutenticationTest.php
+++ tests/Models/AutenticationTest.php
@@ -2,6 +2,7 @@
 use \PHPUnit\Framework\TestCase;
 use App\Models\Autentication;
 use App\Models\Orientador;
+use App\Models\Orientando;
 use App\Models\Connection;
 use App\Utils\Errors\IncorrectLoginException;
 
@@ -16,27 +17,40 @@ class AutenticationTest extends TestCase
 
         try {
             $conn = Connection::getConnection();
-            $conn->query("INSERT INTO pessoa (rg, cpf, dataNasc, nome, email) VALUES ('123456789','12345678901', '1999-12-12', 'Usuario 1', 'email_valido@example.com')");
+
+            $conn->query("INSERT INTO pessoa (rg, cpf, dataNasc, nome, email) VALUES ('123456789','12345678901', '1999-12-12', 'Usuario 1', 'email_valido@orientador.com')");
             $id_pessoa = $conn->lastInsertId();
             $conn->query("INSERT INTO orientador (_cpp, user, senha, id_pessoa) VALUES (1,'Usuario 1', MD5('senha_valida'), {$id_pessoa})");
+            $id_orientador = $conn->lastInsertId();
+
+            $conn->query("INSERT INTO pessoa (rg, cpf, dataNasc, nome, email) VALUES ('234567890','23456789012', '1999-12-12', 'Usuario 2', 'email_valido@orientando.com')");
+            $id_pessoa = $conn->lastInsertId();
+            $conn->query("INSERT INTO orientando (user, senha, id_pessoa, id_orientador) VALUES ('Usuario 2', MD5('senha_valida'), {$id_pessoa}, {$id_orientador})");
+        }
+        catch(PDOException $e) {}
+    }
+
+    private function deletePersonWhereRg(string $rg)
+    {
+        try {
+            $conn = Connection::getConnection();
+            $stmt = $conn->query("SELECT id_pessoa FROM pessoa WHERE rg = {$rg}");
+            $stmt->execute();
+            $fetch = $stmt->fetch();
+            if(isset($fetch["id_pessoa"])) {
+                $id_pessoa = $fetch["id_pessoa"];
+                $conn->query("DELETE FROM orientador WHERE id_pessoa = '{$id_pessoa}'");
+                $conn->query("DELETE FROM orientando WHERE id_pessoa = '{$id_pessoa}'");
+                $conn->query("DELETE FROM pessoa WHERE id_pessoa = {$id_pessoa}");
+            }
         }
         catch(PDOException $e) {}
     }
 
     public function __destruct()
     {
-        try {
-            $conn = Connection::getConnection();
-            $stmt = $conn->query("SELECT id_pessoa FROM pessoa WHERE rg = '123456789'");
-            $stmt->execute();
-            $fetch = $stmt->fetch();
-            if(isset($fetch["id_pessoa"])) {
-                $id_pessoa = $fetch["id_pessoa"];
-                $conn->query("DELETE FROM orientador WHERE id_pessoa = {$id_pessoa}");
-                $conn->query("DELETE FROM pessoa WHERE id_pessoa = {$id_pessoa}");
-            }
-        }
-        catch(PDOException $e) {}
+        $this->deletePersonWhereRg("123456789");
+        $this->deletePersonWhereRg("234567890");
     }
 
     public function testLoginEmailInvalido()
@@ -53,10 +67,17 @@ class AutenticationTest extends TestCase
         $this->assertEquals(IncorrectLoginException::class, $result::class);
     }
 
-    public function testLoginParametrosValidos()
+    public function testLoginParametrosValidosParaOrientador()
     {
-        $result = (new Autentication())->login("email_valido@example.com", "senha_valida");
+        $result = (new Autentication())->login("email_valido@orientador.com", "senha_valida");
 
         $this->assertEquals(Orientador::class, $result::class);
+    }
+
+    public function testLoginParametrosValidosParaOrientando()
+    {
+        $result = (new Autentication())->login("email_valido@orientando.com", "senha_valida");
+
+        $this->assertEquals(Orientando::class, $result::class);
     }
 }
--- tests/unitTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-use PHPUnit\framework\TestCase;
-
-class unitTest extends PHPUnit\Framework\TestCase{
-    public function teste(){
-        $this->assertTrue(false);
-    }
-}
-?>
